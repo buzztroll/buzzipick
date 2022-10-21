8823cf9935d2dc23124f3053cdc03e3ad5a13ce9
John Bresnahan <buzztroll@gmail.com>
2022-10-21 06:45:13 -1000

    Top level exception handler for logging

--- pypicker/buzziphoto/photoframe.py
+++ pypicker/buzziphoto/photoframe.py
@@ -29,30 +29,34 @@ def main():
     p = buzzipick.PhotoPicker(album_dir)
     g_logger.info("Initialize the display")
     i = buzzimage.BuzzScreenImage()
-    mgr = picmgr.PictureManager(p)
-    done = False
-    while not done:
+    try:
+        mgr = picmgr.PictureManager(p)
+        done = False
         delay = std_delay
-        event = i.get_event(timeout=delay)
-        if event == buzzimage.NEXT:
-            mgr.next()
-        elif event == buzzimage.BACK:
-            mgr.back()
-        elif event == buzzimage.FORWARD:
-            mgr.forward()
-        elif event == buzzimage.PAUSE:
-            mgr.pause()
-        photo = mgr.get_current()
+        while not done:
+            event = i.get_event(timeout=delay)
+            if event == buzzimage.NEXT:
+                mgr.next()
+            elif event == buzzimage.BACK:
+                mgr.back()
+            elif event == buzzimage.FORWARD:
+                mgr.forward()
+            elif event == buzzimage.PAUSE:
+                mgr.pause()
+            photo = mgr.get_current()
 
-        logging.info("Show photo %s", photo)
-        try:
-            i.show_picture(photo)
-        except Exception as ex:
-            delay = 0  # if there is a problem get the next photo right away
-            logging.error("Failed to show the photo %s", photo, ex)
-        if event == buzzimage.QUIT:
-            done = True
-    i.done()
+            logging.info("Show photo %s", photo)
+            try:
+                i.show_picture(photo)
+                delay = std_delay
+            except Exception as ex:
+                delay = 0  # if there is a problem get the next photo right away
+                logging.error("Failed to show the photo %s", photo, ex)
+            if event == buzzimage.QUIT:
+                done = True
+    except Exception as ex:
+        g_logger.error("A top level error occurred", ex)
+        i.done()
 
 
 if __name__ == "__main__":
